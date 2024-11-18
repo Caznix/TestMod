da824eaf9774172efb53fa7f42926c81bbc52e74
Caznix <caznix01@gmail.com>
2024-11-18 01:01:00 -0500

    Initial commit

---
+++ src/main/java/sys/caz/tutorialmod/TutorialModClient.java
@@ -0,0 +1,7 @@
+package sys.caz.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
